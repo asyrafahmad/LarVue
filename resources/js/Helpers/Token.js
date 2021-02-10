f2c4799d7504991578c404f95ed8b2d37a88fd0d
Asyraf <are.syraf97@yahoo.com>
2021-02-11 00:32:30 +0800

    create helper token

---
+++ resources/js/Helpers/Token.js
@@ -0,0 +1,27 @@
+class Token{
+
+    // to validate token
+    isValid(token){
+
+        const payload = this.payload(token)
+
+        if(payload){
+            return payload.iss = "http://127.0.0.1:8000/api/auth/login" || "http://larvueproject.test/api/auth/login" || "http://127.0.0.1:8000/api/auth/signup" ? true : false
+        }
+
+        return false
+    }
+
+    payload(token){
+
+        const payload = token.split('.')[1]             // get split 1 part from token 
+
+        return this.decode(payload);                    //decode payload
+    }
+
+    decode(payload){
+        return JSON.parse(atob(payload))                // atob = encrypt/decode
+    }
+}
+
+export default Token = new Token ();

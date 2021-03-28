feee380ae600372a3f88c5b3505850d537f94e54
Asyraf <are.syraf97@yahoo.com>
2021-03-28 11:01:37 +0800

    [Add] expense module

---
+++ app/Models/Model/Expense.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    protected $fillable = [
+        'details', 'amount'
+    ];
+}
